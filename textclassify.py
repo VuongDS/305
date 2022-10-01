b239a76dc897921a02faef0fc4e320480e0d0f05
VuongDS <95429735+VuongDS@users.noreply.github.com>
2022-10-01 17:15:16 +0700

    Update textclassify.py

--- textclassify.py
+++ textclassify.py
@@ -11,9 +11,6 @@ Original file is located at
 ## Import library
 """
 
-!pip3 -q install fasttext
-!pip3 -q install wordcloud
-
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
